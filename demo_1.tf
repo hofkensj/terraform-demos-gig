6f6c0e534325bd2d1f58da53d709b5fb1259d99c
Jurgen Hofkens <jurgen@gig.tech>
2018-10-07 08:44:15 +0100

    solved bug

--- demo_1.tf
+++ demo_1.tf
@@ -1,5 +1,5 @@
 provider "jumpscale" {
-  server_url = "https://be-gen-1.demo.greenitglobe.com"
+  server_url = "https://be-gen-1.demo.greenitglobe.com/restmachine"
   client_id = "faF2k31kwSMVGXrD1-X246oVlQoD"
   client_secret = "wQkcWueAeLQzR9x2ZyWDiQANg2Ox"
 }
