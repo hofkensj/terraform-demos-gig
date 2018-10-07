2003ef7caceb7fdf54f42ab96e66d440ba04911d
Jurgen Hofkens <jurgen@gig.tech>
2018-10-07 11:32:02 +0100

    updating full kub cluster setup

--- demo_1.tf
+++ demo_1.tf
@@ -4,11 +4,60 @@ provider "jumpscale" {
   client_secret = "wQkcWueAeLQzR9x2ZyWDiQANg2Ox"
 }
 
-resource "jumpscale_machine" "machine" {
+# Creating the necessary machines in the primary cloudspace
+
+resource "jumpscale_machine" "Kub_Master_1" {
   cloudspace_id = "389"
   image_id = 5
   size_id = 1
   disksize = 10
-  name = "MyJumpscaleMachine"
-  description = "Jumpscale Machine Provisioned With Terraform"
+  name = "Kubernetes-Master-1"
+  description = "Master 1 for Kubernetes Cluster in Cloudspace"
+}
+
+resource "jumpscale_machine" "Kub_Master_2" {
+  cloudspace_id = "389"
+  image_id = 5
+  size_id = 1
+  disksize = 10
+  name = "Kubernetes-Master-2"
+  description = "Master 2 for Kubernetes Cluster in Cloudspace"
+}
+
+resource "jumpscale_machine" "Kub_Slave_1" {
+  cloudspace_id = "389"
+  image_id = 5
+  size_id = 1
+  disksize = 10
+  name = "Kubernetes-Slave-1"
+  description = "Slave 1 for Kubernetes Cluster in Cloudspace"
+}
+
+resource "jumpscale_machine" "Kub_Slave_2" {
+  cloudspace_id = "389"
+  image_id = 5
+  size_id = 1
+  disksize = 10
+  name = "Kubernetes-Slave-2"
+  description = "Slave 2 for Kubernetes Cluster in Cloudspace"
+}
+
+# Setting up the appropriate port forwards
+
+resource "jumpscale_port_forwarding" "port_forward_ssh_Kub_Master_1" {
+  cloudspace_id = "389"
+  public_ip = "195.134.212.11"
+  public_port = 2201
+  machine_id = "${jumpscale_machine.Kub_Master_1.id}"
+  local_port = 22
+  protocol = "tcp"
+}
+
+resource "jumpscale_port_forwarding" "port_forward_ssh_Kub_Master_2" {
+  cloudspace_id = "389"
+  public_ip = "195.134.212.11"
+  public_port = 2202
+  machine_id = "${jumpscale_machine.Kub_Master_2.id}"
+  local_port = 22
+  protocol = "tcp"
 }
